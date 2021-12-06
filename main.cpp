708f6fa184d07e963cc98e549647449660e6242e
Gabriel <75862004+Gabriel1961@users.noreply.github.com>
2021-12-06 22:37:54 +0200

    Ip

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "RenderLoop.h"
 
-std::string uri = "ws://79.114.95.69:9002";
 #include "Physics/Collider2D.h"
 
 int main(void)
 {
+	std::string uri;
+	std::cin >> uri;
+	if (uri == "")
+		uri = "ws://79.114.95.69:9002";
+	else
+		uri = "ws://" + uri + ":9002";
 	Client client(uri);
 	auto f = std::async(&Client::ConnectToServer, &client);
 	RenderLoop(client);
